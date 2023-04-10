4a72baaf992359b029d6fbc975815403ae96c234
Ivanitskyi Oleksii <goleecindj@gmail.com>
2023-04-10 21:10:42 +0300

    fix link import

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,7 +3,6 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import logo from 'assets/logo4.png'
-import { Link } from 'react-router-dom';
 import Menu from 'components/Menu/Menu'
 
 type Props = {}
