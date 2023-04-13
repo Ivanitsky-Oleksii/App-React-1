f9a00ded7c37621e27454b0eb58a17c25473b47e
Ivanitskyi Oleksii <goleecindj@gmail.com>
2023-04-13 16:15:32 +0300

    fix font size in about page

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -5,8 +5,8 @@ type Props = {}
 const About = (props: Props) => {
     return (
         <div className="about-btn">
-    <h3 className='text-1'>Hello everybody!</h3>
-    <p className='text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus labore, quae qui beatae, vitae earum maiores adipisci aspernatur delectus omnis perferendis. Illo esse dignissimos eos blanditiis corporis, sint architecto molestiae?</p>
+    <h3 className='textab-1'>Hello everybody!</h3>
+    <p className='textab'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus labore, quae qui beatae, vitae earum maiores adipisci aspernatur delectus omnis perferendis. Illo esse dignissimos eos blanditiis corporis, sint architecto molestiae?</p>
         </div>
     )
 }
